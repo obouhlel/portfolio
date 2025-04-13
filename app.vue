347ff05d4d7fd970660f46876e9bdea95f662104
Oussama BOUHLEL <oussama_bouhlel@outlook.fr>
2025-04-13 21:04:35 +0200

    Update the page and package json install tailwind

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
 <script setup lang="ts">
-import { NuxtPage } from '#components';
+import { NuxtPage } from "#components";
+import { useHead } from "@unhead/vue";
+
+useHead({
+  title: "Oussama BOUHLEL - Portfolio",
+  meta: [
+    { name: "description", content: "Oussama BOUHLEL porfolio" },
+    { name: "keywords", content: "obouhlel portofolio developer fullstack" },
+  ],
+});
 </script>
 
 <template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
-    <h1>Oussama BOUHLEL</h1>
+  <div class="flex w-full h-screen items-center justify-center">
+    <h1 class="text-9xl">Working in progress</h1>
   </div>
 </template>
