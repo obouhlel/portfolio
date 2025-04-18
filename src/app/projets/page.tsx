5f920cfbfbfe17d91ee5c80207b96b95756bafa6
Oussama BOUHLEL <oussama_bouhlel@outlook.fr>
2025-04-18 15:40:21 +0200

    Set Project page in progress

--- src/app/projets/page.tsx
+++ src/app/projets/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { ProjectCard } from "@/components/ui/project-card";
+// import { ProjectCard } from "@/components/ui/project-card";
 
 export default function Page() {
   return (
@@ -9,18 +9,27 @@ export default function Page() {
         <h1 className="text-5xl font-bold text-white text-center mb-8">
           Mes projets
         </h1>
-        <ProjectCard items={projects} />
+        {/* <ProjectCard items={projects} /> */}
+        <div className="bg-gray-800 p-6 rounded-lg shadow-md text-center">
+          <h2 className="text-2xl font-semibold text-white mb-2">
+            En cours de développement
+          </h2>
+          <p className="text-gray-300">
+            Cette section est actuellement en construction. Revenez bientôt pour
+            découvrir mes projets !
+          </p>
+        </div>
       </div>
     </>
   );
 }
 
-export const projects = [
-  {
-    title: "Hypertube",
-    description: "Description du projet",
-    image: "/images/hypertube.png",
-    link: "https://hypertube.obouhlel.xyz",
-    stack: ["Node.js", "AdonisJS", "Inertia.js", "TypeScript", "React"],
-  },
-];
+// const projects = [
+//   {
+//     title: "Hypertube",
+//     description: "Description du projet",
+//     image: "/images/hypertube.png",
+//     link: "https://hypertube.obouhlel.xyz",
+//     stack: ["Node.js", "AdonisJS", "Inertia.js", "TypeScript", "React"],
+//   },
+// ];
