7180552f26bed9cc5d2ffc64828fdeb203c015e6
Oussama BOUHLEL <oussama_bouhlel@outlook.fr>
2025-04-20 11:16:23 +0200

    Update the form for white mode

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -109,13 +109,13 @@ export default function Page() {
               name="message"
               placeholder="Votre message"
               required
-              className="shadow-input dark:placeholder-text-neutral-600 flex h-32 w-full rounded-md border-none bg-gray-50 px-3 py-2 text-sm text-white transition duration-400 placeholder:text-neutral-400 focus-visible:ring-[2px] focus-visible:ring-neutral-400 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 dark:bg-zinc-800 dark:shadow-[0px_0px_1px_1px_#404040] dark:focus-visible:ring-neutral-600"
+              className="shadow-input dark:placeholder-text-neutral-600 flex h-32 w-full rounded-md border-none bg-gray-50 px-3 py-2 text-sm text-black dark:text-white transition duration-400 placeholder:text-neutral-400 focus-visible:ring-[2px] focus-visible:ring-neutral-400 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 dark:bg-zinc-800 dark:shadow-[0px_0px_1px_1px_#404040] dark:focus-visible:ring-neutral-600"
             />
           </div>
           <button
             type="submit"
             disabled={isSubmitting}
-            className="w-full text-black dark:text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 disabled:opacity-70"
+            className="w-full text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 disabled:opacity-70"
           >
             {isSubmitting ? "Envoi en cours..." : "Envoyer"}
           </button>
