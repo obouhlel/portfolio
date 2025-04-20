a6ea1d8c9cc0b233126c2dcf7d51b856e675c49a
Oussama BOUHLEL <oussama_bouhlel@outlook.fr>
2025-04-20 11:04:05 +0200

    Update the form for white mode

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -115,7 +115,7 @@ export default function Page() {
           <button
             type="submit"
             disabled={isSubmitting}
-            className="w-full text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 disabled:opacity-70"
+            className="w-full text-black dark:text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 disabled:opacity-70"
           >
             {isSubmitting ? "Envoi en cours..." : "Envoyer"}
           </button>
